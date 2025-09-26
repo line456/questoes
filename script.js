6b72c1579207cf372bb4250ce4e20bc0a391201f
line456 <aline.s.bezerra@aluno.senai.br>
2025-09-26 10:53:46 -0500

    monte atividade

---
+++ script.js
@@ -0,0 +1,14 @@
+function somarDoisNumeros() {
+  
+  let numero1 = Number(prompt("Digite o primeiro número:"));
+  let numero2 = Number(prompt("Digite o segundo número:"));
+
+ 
+  let soma = numero1 + numero2;
+
+ 
+  alert("A soma dos dois números é: " + soma);
+}
+
+
+somarDoisNumeros();
